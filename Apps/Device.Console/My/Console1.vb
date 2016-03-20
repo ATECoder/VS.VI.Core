ce452ac59a5736fc72b62563807fa795b34b5418
David Hary <david@isr.cc>
2016-03-19 20:21:10 -0700

    Device Console1: Add compile conditions to select the control panel.

--- Apps/Device.Console/My/Console1.vb
+++ Apps/Device.Console/My/Console1.vb
@@ -2,9 +2,21 @@
     Inherits isr.VI.Instrument.InstrumentPanelForm
     <CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")>
     Protected Overrides Sub OnLoad(e As EventArgs)
-        ' Me.AddInstrumentPanel("Switch/DMM", New VI.Tsp.K3700.K3700Panel)
+        Dim enabled As Boolean = False
+#If False Then
+#ElseIf K2400 Then
         Me.AddInstrumentPanel("SourceMeter", New VI.K2400.K2400Panel)
-        Me.AddListeners(My.Application.MyLog)
-        MyBase.OnLoad(e)
+        enabled = true
+#ElseIf K3700 Then
+        Me.AddInstrumentPanel("Switch/DMM", New VI.Tsp.K3700.K3700Panel)
+        enabled = true
+#ElseIf K7000 Then
+        Me.AddInstrumentPanel("Switch", New VI.K7000.K7000Panel)
+        enabled = True
+#End If
+        If enabled Then
+            Me.AddListeners(My.Application.MyLog)
+            MyBase.OnLoad(e)
+        End If
     End Sub
 End Class
