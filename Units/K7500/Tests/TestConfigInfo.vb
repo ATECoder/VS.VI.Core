170d4efaad61e817fa34fc0c3a5805e42ab7e4d5
David Hary <david@isr.cc>
2017-10-13 11:30:00 -0700

    Units:
    Adds partial test info for validating the test context and tracing.

--- Units/K7500/Tests/TestConfigInfo.vb
+++ Units/K7500/Tests/TestConfigInfo.vb
@@ -1,16 +1,6 @@
 ﻿Imports System.Configuration
-''' <summary> Information about the test. </summary>
-''' <license>
-''' (c) 2017 Integrated Scientific Resources, Inc. All rights reserved.<para>
-''' Licensed under The MIT License.</para><para>
-''' THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-''' BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-''' NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-''' DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-''' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</para>
-''' </license>
-''' <history date="10/13/2017" by="David" revision=""> Created. </history>
-Public NotInheritable Class TestInfo
+
+Partial Public NotInheritable Class TestInfo
 
 #Region " CONSTRUCTOR "
 
